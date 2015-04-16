598d373a097875176c0f60eb768aad99e80c682e
한수지 <@192.168.56.1>
2015-04-16 12:48:33 +0900

    제출

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -26,7 +26,9 @@ public class CarTest {
 //		Car car2 = new Car();
 //		Car car3 = new Car();
 		
-		Car2 car = new Car2();
+//		Car2 car = new Car2();
+		
+		Car3 car = new Car3();
 		
 		System.out.println("<< 자동차 목록 >>");
 		
@@ -36,14 +38,16 @@ public class CarTest {
 		System.out.println("최대속도 : " + car.getMaxSpeed() + "km" );
 		System.out.println("가격 : " + String.format("%,d", car.getPrice() ) + "원");
 		System.out.println();
-		
-		car = new Car2("기아 자동차", "K7", "흰색", 246, 40000000);
-		
+
 //		car2.setCompany("기아자동차");
 //		car2.setModel("K7");
 //		car2.setColor("흰색");
 //		car2.setMaxSpeed(246);
-//		car2.setPrice(40000000);
+//		car2.setPrice(40000000);	
+		
+//		car = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+		
+		car = new Car3("기아자동차", "K7", "흰색", 246);
 
 		System.out.println("제조사명 : " + car.getCompany()); 
 		System.out.println("모델명 : " + car.getModel());
@@ -52,7 +56,7 @@ public class CarTest {
 		System.out.println("가격 : " + String.format("%,d", car.getPrice()) + "원");
 		System.out.println();
 		
-		car = new Car2("삼성 자동차", "SM7", "회색", 200, 38000000);
+//		car = new Car2("삼성 자동차", "SM7", "회색", 200, 38000000);
 
 //		car3.setCompany("삼성자동차");
 //		car3.setModel("SM7");
@@ -60,6 +64,8 @@ public class CarTest {
 //		car3.setMaxSpeed(200);
 //		car3.setPrice(38000000);
 
+		car = new Car3("삼성자통차", "SM7", "회색");
+		
 		System.out.println("제조사명 : " + car.getCompany()); 
 		System.out.println("모델명 : " + car.getModel());
 		System.out.println("색상 : " + car.getColor());
