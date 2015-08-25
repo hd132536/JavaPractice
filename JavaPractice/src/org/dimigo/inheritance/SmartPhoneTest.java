58ed7a51df2b9a1f2f144d0f150ef1906eaf80ab
한수지 <@192.168.56.1>
2015-08-25 10:43:22 +0900

    rr

--- JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -22,7 +22,7 @@ public class SmartPhoneTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SmartPhone a1 = new IPhone("IPhone 6", "애플", 7000000);
+		SmartPhone a1 = new IPhone("IPhone 6", "애플", 700000);
 		SmartPhone g1 = new Galaxy("갤럭시 S6", "삼성", 650000);
 		
 		System.out.println(a1);
