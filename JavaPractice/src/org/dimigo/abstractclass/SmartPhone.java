14c9924eac18b8f3e35071843caa7389d4c2159d
한수지 <@192.168.56.1>
2015-09-01 10:16:07 +0900

    d

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhone.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * |_ SmartPhone
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 25.
+ * </pre>
+ *
+ * @author		: 한수지
+ * @vision		: 1.0
+ */
+public abstract class SmartPhone {
+	private String model;
+	private String company;
+	private int price;
+	
+	public SmartPhone() {
+		
+	}
+	
+	public SmartPhone(String model, String company, int price) {
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+
+	public void turnOn() {
+		System.out.println(model + "의 전원을 켭니다.");
+	}
+	
+	public void turnOff() {
+		System.out.println(model + "의 전원을 끕니다.");
+	}
+	
+	public abstract void pay();
+	
+	public void useSpecialFunction(){
+		if(this == null) return;
+		if(this instanceof IPhone) {
+			IPhone i = new IPhone();
+			i.useAirDrop();
+		}
+		else if(this instanceof Galaxy) {
+			Galaxy g = new Galaxy();
+			g.useWirelessCharging();
+		}
+	}
+
+	public String toString() {
+		return "모엘명 : " + model + ", 제조사 : " + company + ", 가격 : " + String.format("%,1d", price);
+	}
+}
---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * |_ SmartPhoneTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 8. 25.
+ * </pre>
+ *
+ * @author		: 한수지
+ * @vision		: 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SmartPhone a1 = new IPhone("IPhone 6", "애플", 700000);
+		SmartPhone g1 = new Galaxy("갤럭시 S6", "삼성", 650000);
+		
+		System.out.println(a1);
+		System.out.println(g1);
+		
+		System.out.println();
+		
+		a1.turnOn();
+		a1.pay();
+		a1.useSpecialFunction();
+		a1.turnOff();
+		
+		System.out.println();
+		
+		g1.turnOn();
+		g1.pay();
+		g1.useSpecialFunction();
+		g1.turnOff();
+	}
+
+}
