a0bcf796a57adf37a33f51a78bb518b24533726a
한수지 <@192.168.56.1>
2015-11-04 15:34:11 +0900

    aaaaaa

---
+++ JavaPractice/src/org/dimigo/thread/Race.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ * |_ Race
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 11. 4.
+ * </pre>
+ *
+ * @author		: 한수지
+ * @vision		: 1.0
+ */
+public class Race {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("main method start");
+		
+		Thread t1 = new Runner("한수지");
+		Thread t2 = new Runner("짱느려");
+		
+		t1.start();
+		t2.start();
+		
+		System.out.println("main method end");
+
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/thread/Race2.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ * |_ Race
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 11. 4.
+ * </pre>
+ *
+ * @author		: 한수지
+ * @vision		: 1.0
+ */
+public class Race2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("main method start");
+		
+		Thread t1 = new Thread(new Runner("수지"));
+		Thread t2 = new Thread(new Runner("하이"));
+		
+		t1.start();
+		t2.start();
+		
+		System.out.println("main method end");
+
+	}
+
+}
